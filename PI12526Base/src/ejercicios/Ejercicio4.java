e63ddedb7e8439c440618c10e89d28ef02e8681a
luicamchu <luicamchu@alum.us.es>
2025-11-02 13:17:48 +0100

    upd

--- PI12526Base/src/ejercicios/Ejercicio4.java
+++ PI12526Base/src/ejercicios/Ejercicio4.java
@@ -27,20 +27,6 @@ public class Ejercicio4 {
 			return recFinal(nuevoAcumulador, a-2);
 		}
 	}
-
-	
-	public static BigInteger funcRecBig(Integer a) {
-		//BigInteger bigNum1 = new BigInteger("123456789012345678901234567890");
-		//long smallNum = 100L;
-		//BigInteger bigNum2 = BigInteger.valueOf(smallNum);
-		BigInteger r = BigInteger.ONE;
-		if(a<10) {
-			r = BigInteger.valueOf(5L);   // Representa el número 1;
-		} else {
-			r = BigInteger.valueOf((long) Math.sqrt(3*a)).multiply(funcRecBig(a-2));
-		}
-		return r;
-	}
 	
 	public static Double funcItDouble(Integer a) {
 		Double acumulador = 1.;
@@ -49,6 +35,35 @@ public class Ejercicio4 {
 			a-=2;
 		}
 		return acumulador * 5.;
+	}
+	
+	//BigInteger bigNum1 = new BigInteger("123456789012345678901234567890");
+	//long smallNum = 100L;
+	//BigInteger bigNum2 = BigInteger.valueOf(smallNum);
+	//BigInteger bigNum3 = BigInteger.ONE;
+
+	public static BigInteger funcRecBig(Integer a) {
+		BigInteger r = BigInteger.valueOf(1L);
+		if(a<10) {
+			r = BigInteger.valueOf(5L);   // Representa el número 1;
+		} else {
+			r = BigInteger.valueOf((long) Math.sqrt(3*a)).multiply(funcRecBig(a-2));
+		}
+		return r;
+	}
+	
+	public static BigInteger funcRecFinalBigInteger(Integer a) {
+		BigInteger acumulador = BigInteger.valueOf(1L);
+		return recFinal(acumulador, a);
+	}
+	
+	private static BigInteger recFinal(BigInteger acumulador, Integer a) {
+		if(a<10) {
+			return acumulador.multiply(BigInteger.valueOf(5L));
+		} else {
+			BigInteger nuevoAcumulador = acumulador.multiply(BigInteger.valueOf((long) Math.sqrt(3*a)));
+			return recFinal(nuevoAcumulador, a-2);
+		}
 	}
 	
 	public static BigInteger funcItBig(Integer a) {
--- PI12526Base/src/tests/TestEjercicio4.java
+++ PI12526Base/src/tests/TestEjercicio4.java
@@ -8,6 +8,7 @@ import java.util.stream.IntStream;
 
 import org.apache.commons.math3.fitting.WeightedObservedPoint;
 
+
 import ejercicios.Ejercicio4;
 import us.lsi.common.Pair;
 import us.lsi.common.String2;
@@ -18,24 +19,69 @@ import us.lsi.curvefitting.PowerLog;
 import us.lsi.graphics.MatPlotLib;
 
 public class TestEjercicio4 {
-	private static Integer nMin  = 100;
-	private static Integer nMax  = 1000;
-	private static Integer razon  = 3330;
-	private static Integer nIter  = 50;
-	private static Integer nIterWarmup  = 10000;
+	private static Integer nMin = 100; // n mínimo para el cálculo de potencia
+	private static Integer nMax = 100000; // n máximo para el cálculo de potencia
+	private static Integer razon = 3330; // incremento en los valores de n del cálculo de potencia
+	private static Integer nIter = 50; // número de iteraciones para cada medición de tiempo
+	private static Integer nIterWarmup = 10000; // número de iteraciones para warmup
 
 	private static Double a = 3.;
 	
-	public static void genData (Consumer<Integer> algorith, String file) {
-		Function<Integer, Long> f1 = GenData.time(algorith);
-		GenData.tiemposEjecucionAritmetica(f1, file, nMin, nMax, nMax, nIter, nIterWarmup);
+	public static void genData (Consumer<Integer> algorithm, String file) {
+		Function<Integer,Long> f1 = GenData.time(algorithm);
+		GenData.tiemposEjecucionAritmetica(f1,file,nMin,nMax,razon,nIter,nIterWarmup);
 	}
 	
+	public static void show(Fit pl, String file, String label) {
+		List<WeightedObservedPoint> data = DataFile.points(file);
+		pl.fit(data);
+		MatPlotLib.show(file, pl.getFunction(), String.format("%s = %s",label,pl.getExpression()));
+	}
 	
-	
+	public static void showCombined() {
+		MatPlotLib.showCombined("Tiempos",
+				List.of("ficheros_generados/funcRecDouble.txt",
+						"ficheros_generados/funcRecFinalDouble.txt",
+						"ficheros_generados/funcItDouble.txt",
+						"ficheros_generados/funcRecBig.txt",
+						"ficheros_generados/funcRecFinalBigInteger.txt",
+						"ficheros_generados/funcItBig.txt"), 
+				List.of("Recursiva","RecursivaFinal","Iterativa",
+						"RecursivaBI","RecursivaFinalBI","IterativaBI","Log"));
+	}
+
 	public static void main(String[] args) {
-		genData(t -> Ejemplo4.potenciaR(a, t), "ficheros_generados/p1/porRecursiva.txt");
-		}
+		String nombreArchivo1 = "ficheros_generados/funcRecDouble.txt";
+		String nombreArchivo2 = "ficheros_generados/funcRecFinalDouble.txt";
+		String nombreArchivo3 = "ficheros_generados/funcItDouble.txt";
+		
+		String nombreArchivo4 = "ficheros_generados/funcRecBig.txt";
+		String nombreArchivo5 = "ficheros_generados/funcRecFinalBigInteger.txt";
+		String nombreArchivo6 = "ficheros_generados/funcItBig.txt";
+		
+		genData(t -> Ejercicio4.funcRecDouble(t),nombreArchivo1);
+		genData(t -> Ejercicio4.funcRecFinalDouble(t),nombreArchivo2);
+		genData(t -> Ejercicio4.funcItDouble(t),nombreArchivo3);
+		
+		genData(t -> Ejercicio4.funcRecBig(t),nombreArchivo4);
+		genData(t -> Ejercicio4.funcRecFinalBigInteger(t),nombreArchivo5);
+		genData(t -> Ejercicio4.funcItBig(t),nombreArchivo6);
+		
+
+		show(PowerLog.of(List.of(Pair.of(2, 0.),Pair.of(3, 0.))), nombreArchivo1,"Recursiva");
+		show(PowerLog.of(List.of(Pair.of(2, 0.),Pair.of(3, 0.))), nombreArchivo2,"RecursivaFinal");
+		show(PowerLog.of(List.of(Pair.of(2, 0.),Pair.of(3, 0.))), nombreArchivo3,"Iterativa");
+		
+		show(PowerLog.of(List.of(Pair.of(2, 0.),Pair.of(3, 0.))), nombreArchivo1,"RecursivaBI");
+		show(PowerLog.of(List.of(Pair.of(2, 0.),Pair.of(3, 0.))), nombreArchivo2,"RecursivaFinalBI");
+		show(PowerLog.of(List.of(Pair.of(2, 0.),Pair.of(3, 0.))), nombreArchivo3,"IterativaBI");
+		
+		//show(PowerLog.of(List.of(Pair.of(1, 0.),Pair.of(2, 1.),Pair.of(3, 0.))), nombreArchivo3,"Log");
+		
+		
+		
+		showCombined();
+	}
 
 	
 }
